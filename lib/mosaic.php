8c429be41e172333ec9398a1522e23095d26cc5c
Dylan Lang <dylanlang@Dylans-MacBook-Pro.local>
2021-02-23 19:01:16 -0500

    update

--- lib/mosaic.php
+++ lib/mosaic.php
@@ -18,7 +18,7 @@ namespace mosaic {
             $params = [
                 'client_id=8ef01039251f4b9a8a213ae17ef0e570',
                 'response_type=code',
-                'redirect_uri=https://mosaic.paos.io/create.php',
+                'redirect_uri=https://dev.plumeware.com/mosaic-dev/create.php',
                 'scope=playlist-read-private%20playlist-read-collaborative%20user-top-read'
             ];
 
@@ -42,13 +42,13 @@ namespace mosaic {
                 $params = [
                     "code=$code",
                     "grant_type=$type",
-                    'redirect_uri=https://mosaic.paos.io/create.php'
+                    'redirect_uri=https://dev.plumeware.com/mosaic-dev/create.php'
                 ];
             } else {
                 $params = [
                     "refresh_token=$code",
                     "grant_type=$type",
-                    'redirect_uri=https://mosaic.paos.io/create.php'
+                    'redirect_uri=https://dev.plumeware.com/mosaic-dev/create.php'
                 ];
             }
 
